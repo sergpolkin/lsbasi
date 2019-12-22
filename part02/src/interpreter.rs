8b6016b1ec5b406437343e1a0b4b9b03753980ec
serg <sergpolkin@gmail.com>
2019-12-22 16:20:24 +0300

    part02: multiplication and division

---
+++ part02/src/interpreter.rs
@@ -0,0 +1,149 @@
+
+#[derive(Copy, Clone)]
+enum ArithmeticOp {
+    Plus,
+    Minus,
+    Mul,
+    Div,
+}
+
+enum Token {
+    Integer(i32),
+    Op(ArithmeticOp),
+    EOF,
+}
+
+pub struct Interpreter {
+    text: String,
+    pos: usize,
+    cur_token: Option<Token>,
+}
+
+impl Interpreter {
+    pub fn new<S: Into<String>>(text: S) -> Interpreter {
+        Interpreter {
+            text: text.into(),
+            pos: 0,
+            cur_token: None,
+        }
+    }
+
+    fn parse_integer(&mut self) -> Option<i32> {
+        let text: Vec<char> = self.text.chars().collect();
+        let mut store: Vec<char> = Vec::new();
+        loop {
+            if self.pos > self.text.len() - 1 {
+                let res: String = store.into_iter().collect();
+                return res.parse().ok();
+            }
+
+            let c = text[self.pos];
+            if c.is_digit(10) {
+                store.push(c);
+                self.pos += 1;
+                continue;
+            }
+            else {
+                let res: String = store.into_iter().collect();
+                return res.parse().ok();
+            }
+        }
+    }
+
+    fn get_next_token(&mut self) -> Option<Token> {
+        let text: Vec<char> = self.text.chars().collect();
+
+        loop {
+            if self.pos > self.text.len() - 1 {
+                return Some(Token::EOF);
+            }
+
+            let c = text[self.pos];
+
+            if c.is_whitespace() {
+                self.pos += 1;
+                continue;
+            }
+
+            if c.is_digit(10) {
+                let num = self.parse_integer().unwrap();
+                return Some(Token::Integer(num));
+            }
+
+            if c == '+' {
+                self.pos += 1;
+                return Some(Token::Op(ArithmeticOp::Plus));
+            }
+            if c == '-' {
+                self.pos += 1;
+                return Some(Token::Op(ArithmeticOp::Minus));
+            }
+            if c == '*' {
+                self.pos += 1;
+                return Some(Token::Op(ArithmeticOp::Mul));
+            }
+            if c == '/' {
+                self.pos += 1;
+                return Some(Token::Op(ArithmeticOp::Div));
+            }
+
+            if String::from("\r\n").find(c).is_some() {
+                self.pos += 1;
+                return Some(Token::EOF);
+            }
+
+        }
+    }
+
+    fn unwrap_integer(&self) -> i32 {
+        match self.cur_token {
+            Some(Token::Integer(n)) => n,
+            _ => panic!("Expect integer at {}", self.pos)
+        }
+    }
+
+    fn unwrap_op(&self) -> Option<ArithmeticOp> {
+        match self.cur_token {
+            Some(Token::Op(op)) => Some(op),
+            _ => panic!("Expect arithmetic operator at {}", self.pos)
+        }
+    }
+
+    fn unwrap_eof(&self) {
+        match self.cur_token {
+            Some(Token::EOF) => (),
+            _ => panic!("Expect EOF token at {}", self.pos)
+        }
+    }
+
+    pub fn exec(&mut self) -> i32 {
+        self.cur_token = self.get_next_token();
+        let left = self.unwrap_integer();
+
+        self.cur_token = self.get_next_token();
+        let op = self.unwrap_op();
+
+        self.cur_token = self.get_next_token();
+        let right = self.unwrap_integer();
+
+        self.cur_token = self.get_next_token();
+        self.unwrap_eof();
+
+        match op {
+            Some(ArithmeticOp::Plus)  => left + right,
+            Some(ArithmeticOp::Minus) => left - right,
+            Some(ArithmeticOp::Mul)   => left * right,
+            Some(ArithmeticOp::Div)   => left / right,
+            _ => unreachable!()
+        }
+    }
+}
+
+#[test]
+fn interpreter_tests() {
+    assert_eq!(Interpreter::new("3+5").exec(), 8);
+    assert_eq!(Interpreter::new("12+3").exec(), 15);
+    assert_eq!(Interpreter::new(" 12+3").exec(), 15);
+    assert_eq!(Interpreter::new("7 - 5").exec(), 2);
+    assert_eq!(Interpreter::new("5 - 7").exec(), -2);
+}
